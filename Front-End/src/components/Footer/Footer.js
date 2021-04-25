3f51f7860290a30c52275498e98f133225d83f4d
tuliotroche <tgtroche@gmail.com>
2021-04-25 09:24:57 -0400

    Footer added

--- Front-End/src/components/Footer/Footer.js
+++ Front-End/src/components/Footer/Footer.js
@@ -1,9 +1,13 @@
 import React from "react";
+import classes from './Footer.module.css'
 
 const Footer = () => (
-    <p>
-        Footer
-    </p>
+    <div className={classes.footer}>
+        <div>Have a suggestion? <u>Let me know</u> what you think would make this app better!<br />
+
+            Built in React, Node/Express with ☕ and ❤, launched Apr 25th 2021.<br />
+            For contact/hiring go to <u>www.GerellTroche.com</u></div>
+    </div>
 )
 
 export default Footer
